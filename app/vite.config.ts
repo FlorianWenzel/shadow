3731aaed535fd15d063729d57f0a9a5ed30960a1
Florian Wenzel <mail@florian-wenzel.io>
2024-06-09 12:34:19 +0200

    feat(app): add backend env variable

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://backend:3000',
+        target: process.env.BACKEND_URL || 'http://localhost:3000',
         changeOrigin: true
       }
     }
